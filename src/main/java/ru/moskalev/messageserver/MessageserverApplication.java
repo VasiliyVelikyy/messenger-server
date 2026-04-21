0581333f46e9da256cac9a8ef2f91c4b4c2a14c3
VasiliyVelikyy <algoritmick.vm@gmail.com>
2026-04-21 21:02:50 +0300

    init project

---
+++ src/main/java/ru/moskalev/messageserver/MessageserverApplication.java
@@ -0,0 +1,13 @@
+package ru.moskalev.messageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/moskalev/messageserver/MessageserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.moskalev.messageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
